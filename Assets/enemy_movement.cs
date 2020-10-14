d2a8fe2d3d0d1541add1d33c10432e1f5edeaeb9
KIVUOS <sarkar.souvik2016@example.com>
2020-10-14 14:05:50 +0530

    initial

---
+++ Assets/enemy_movement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemy_movement : MonoBehaviour
+{
+    public float rotation_speed = 5f;
+    public float dis = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * rotation_speed * Time.deltaTime);
+        RaycastHit2D hit_info = Physics2D.Raycast(transform.position, transform.right, dis);
+        if (hit_info.collider != null)
+        {
+            Debug.DrawLine(transform.position, hit_info.point, Color.red);
+            if(hit_info.collider.CompareTag("Player"))
+            {
+                Destroy(hit_info.collider.gameObject);
+            }
+        }
+        else
+        {
+            Debug.DrawLine(transform.position, transform.position + transform.right*dis, Color.green);
+        }
+    }
+}
---
+++ Assets/movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+
+    public float speed = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        if(x != 0 || y != 0)
+        {
+            move(x, y);
+        }
+    }
+
+    public void move(float x, float y)
+    {
+        transform.Translate(new Vector2(x, y).normalized * speed * Time.deltaTime);
+    }
+}
